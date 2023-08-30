cb960f8e8f8435e080071a750ce6ac54da2bf0e0
Brandon Campbell-Kearns <brandon.j.kearns@gmail.com>
2023-08-29 20:55:44 -0400

    Implements recursive splitting of text from documents generated from a PDF

--- document-splitting/recursive-splitting-pdf.py
+++ document-splitting/recursive-splitting-pdf.py
@@ -4,10 +4,16 @@ from langchain.text_splitter import CharacterTextSplitter
 loader = PyPDFLoader("prayer-project.pdf")
 pages = loader.load()
 
-text_spliter = CharacterTextSplitter(
+text_splitter = CharacterTextSplitter(
     separator="\n",
     chunk_size=1000,
     chunk_overlap=150,
     length_function=len
 )
 
+docs = text_splitter.split_documents(pages)
+
+print("Number of documents", len(docs))
+print("Number of pages", len(pages))
+
+print("first chunk", docs[0:2])
